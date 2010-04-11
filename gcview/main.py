2fc0ba8af46b0edbab0ca0dbf7369b58da903296
Alex Teiche <xelapond@gmail.com>
2010-04-10 23:17:12 -0700

    *Moved constant color stuff to constants.py *Moved the drawing functions that draw actual shapes(not manipulate the viewport or generate display lists) to draw.py

--- gcview/main.py
+++ gcview/main.py
@@ -6,6 +6,7 @@ import pyglet
 from pyglet.gl import *
 
 import parse
+import draw
 
 #Zoom level
 gzl = 1
@@ -22,58 +23,6 @@ cdict = {
      'EXCEPTION' : (1, 1, 1)
 }
 
-def draw_vertex(args):
-    k = args.keys()
-    '''
-    Lots of stuff happens here.
-    I don't want to explain how this works, it was too much fun to write.
-    That would ruin the fun.
-    
-    What this does is see if there is an X, Y and Z coordinate.  
-    It puts them in a list; [X, Y, Z].
-    If any of them are missing, it checks for an old coordinate(from the last command(OX, OY or OZ).
-    If there is one present, it uses that.
-    If neither the current coordinate or its old counterpart exist, use the name of the coordinate(sans 'O')
-    If there are any strings in the list(meaning there was a missing coordinate) it can't be drawn
-    '''
-    #p = [not (type(g) == str) or ('O' + g) in k for g in 
-    #p = [((not (g[0] == 'O') and args[g])) or ((g in k) and args[g]) or (g[1]) for g in [((x in k) and x) or ('O' + x) for x in ['X', 'Y', 'Z']]]
-    p = ['X', 'Y', 'Z']
-    for cn in p:
-        if cn in k:
-            p[p.index(cn)] = args[cn]
-        else:
-            if 'O' + cn in k:
-                p[p.index(cn)] = args['O' + cn]
-
-    t = filter(lambda x: type(x) == str, p)
-    if t:
-        print 'Could not execute command, no ' + str(t) + ' Coordinate(s)'
-        return False, p
-
-    glVertex2f(p[0]*400, p[1]*400)
-    return True
-    
-def draw_rapid(args):
-    #We just do this so we don't get blending
-    if args['OC'][0] == 'GO1':
-        glEnd()
-        glBegin(GL_LINE_STRIP)
-
-    glColor4f(*RAPID_POS_COLOR)
-
-    return draw_vertex(args)
-
-def draw_lerp(args):
-    #We just do this so we don't get blending
-    if args['OC'][0] == 'GO1':
-        glEnd()
-        glBegin(GL_LINE_STRIP)
-
-    glColor4f(*LERP_COLOR)
-
-    return draw_vertex(args)
-
 def set_imperial(args):
     args['SD']['inches'] = True
 
@@ -88,8 +37,8 @@ def set_incremental(args):
 
 #Maps G-Codes to python functions
 fdict = {
-   'G00' : draw_rapid,
-   'G01' : draw_lerp,
+   'G00' : draw.rapid,
+   'G01' : draw.lerp,
    'G20' : set_imperial,
    'G21' : set_metric,
    'G90' : set_absolute,
@@ -105,15 +54,6 @@ def start_display_list():
     glNewList(dlist, GL_COMPILE)
     
     return dlist
-
-def args2dict(args):
-    '''
-    Converts a G-Code argument list into a dictionary
-
-    Example:
-       ['X1.010', 'Y0.045'] -> {'X':1.010, 'Y':0.045}
-    '''
-    return dict((exp[:1], float(exp[1:])) for exp in args)
 
 def interpret_file(lines, fdict):
     '''
